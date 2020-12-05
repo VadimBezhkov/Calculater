eedcf0dfd66f7e7a2a33dfd475da621ec479fc83
Вадим Бежков <vadimbezhkov3112@gmail.com>
2020-12-05 17:54:46 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("=====Homework one======\n====by Vadim Bezhkov===\n======calculator=======");
+                Console.WriteLine("+ - press eny key 1");
+                Console.WriteLine("- - press eny key 2");
+                Console.WriteLine("* - press eny key 3");
+                Console.WriteLine("/ - press eny key 4");
+                Console.WriteLine("% - press eny key 5");
+                Console.WriteLine("√ - press eny key 6");
+                Console.WriteLine("exit to program - press eny key 7");
+                string chose = Console.ReadLine();
+                Console.Clear();
+                switch (chose)
+                {
+                    case "1":
+                        Console.WriteLine("You CHOSE (1)");
+                        Console.WriteLine("addition operation");
+                        Console.WriteLine("prees any one number");
+                        decimal number1 = Convert.ToDecimal(Console.ReadLine()); 
+                        Console.WriteLine("prees any two number");
+                        decimal number2 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"result one number + two number = {number1+number2}");
+                        Console.WriteLine("CONTINUE PRESS ANY KEY...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "2":
+
+                        Console.WriteLine("You CHOSE (2)");
+                        Console.WriteLine("subtracting numbers");
+                        Console.WriteLine("prees any one number");
+                        decimal number3 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("prees any two number");
+                        decimal number4 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"result one number - two number = {number3 - number4}");
+                        Console.WriteLine("CONTINUE PRESS ANY KEY...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Console.WriteLine("You CHOSE (3)");
+                        Console.WriteLine("multiplication of numbers");
+                        Console.WriteLine("prees any one number");
+                        decimal number5 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("prees any two number");
+                        decimal number6 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"result one number - two number = {number5 * number6}");
+                        Console.WriteLine("CONTINUE PRESS ANY KEY...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        Console.WriteLine("You CHOSE (4)");
+                        Console.WriteLine("quotient numbers");
+                        Console.WriteLine("prees any one number");
+                        decimal number7 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("prees any two number");
+                        decimal number8 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"result one number - two number = {number7 / number8}");
+                        Console.WriteLine("CONTINUE PRESS ANY KEY...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "5":
+
+                        Console.WriteLine("You CHOSE (5)");
+                        Console.WriteLine("procent");
+                        Console.WriteLine("prees any one number");
+                        decimal number9 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("prees any procent");
+                        decimal number10 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"result  percentage of the number = {(number9 * number10)/100}");
+                        Console.WriteLine("CONTINUE PRESS ANY KEY...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "6":
+                        Console.WriteLine("You CHOSE (5)");
+                        Console.WriteLine("√");
+                        Console.WriteLine("prees in t number");
+                        decimal number11 = Convert.ToDecimal(Console.ReadLine());
+                        double number12 = Math.Sqrt(5.25);
+                       Console.WriteLine($"Root from number = {number12}");
+                        Console.WriteLine("CONTINUE PRESS ANY KEY...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+
+                    case "7":
+
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //number input validation method
+        static decimal check()
+        {
+            decimal number;
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var condition = decimal.TryParse(input, out number);
+                if (condition)
+
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Error enter number");
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            //array cell variable
+            int si = 0;
+            decimal[] result = new decimal[5];
+            decimal[] signs = new decimal[100];
+
+            while (true)
+            {
+                Console.Clear();
+
+                // action menu
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("=====Homework one======\n====by Vadim Bezhkov===\n======calculator=======");
+                Console.WriteLine("+ - enter key 1");
+                Console.WriteLine("- - enter key 2");
+                Console.WriteLine("* - enter key 3");
+                Console.WriteLine("/ - enter key 4");
+                Console.WriteLine("% - enter key 5");
+                Console.WriteLine("√ - enter key 6");
+                Console.WriteLine("5 latest results of operations - enter key 7");
+                Console.WriteLine("exit to program - enter key 8");
+                Console.Write("enter key:");
+                
+                string chose = Console.ReadLine();
+                Console.Clear();
+
+                // vybor deystviya
+                switch (chose)
+                {
+                    //addition operation
+                    case "1":
+                        {
+                            Console.ResetColor();
+                            Console.WriteLine("You CHOSE (1)");
+                            Console.WriteLine("addition operation");
+                            Console.WriteLine("Enter number A");
+                            decimal number1 = check();
+
+                            Console.WriteLine("Enter number B");
+                            decimal number2 = check();
+                            result[si] = number1 + number2;
+
+                            // adding the result to the array
+                            si = si + 1;
+
+                            if (si>4)
+                            {
+                                si = 0;
+                            }
+
+                            Console.WriteLine($"result: {number1} + {number2} = {number1 + number2}");
+                            Console.WriteLine("Do you want to repeat enter? yes \nto exit press any button and press enter");
+                            String repeat = Console.ReadLine();
+
+                            while (true)
+                            {
+                                if (repeat == "yes")
+                                {
+
+                                    if (si > 4)
+                                    {
+                                        si = 0;
+                                    }
+
+                                    goto case "1";
+                                }
+
+                                break;
+                            }
+                        }
+
+                        break;
+                    //subtracting numbers
+                    case "2":
+                        {
+                            Console.ResetColor();
+                            Console.WriteLine("You CHOSE (2)");
+                            Console.WriteLine("subtracting numbers");
+                            Console.WriteLine("Enter number A");
+                            decimal number1 = check();
+
+                            Console.WriteLine("Enter number B");
+                            decimal number2 = check();
+                            result[si] = number1 - number2;
+                            si = si + 1;
+
+                            if (si > 4)
+                            {
+                                si = 0;
+                            }
+
+                            Console.WriteLine($"result: {number1} - {number2} = {number1 - number2}");
+                            Console.WriteLine("Do you want to repeat enter? yes \nto exit press any button and press enter");
+                            String repeat = Console.ReadLine();
+
+                            while (true)
+                            {
+                                if (repeat == "yes")
+                                {
+
+                                    if (si > 4)
+                                    {
+                                        si = 0;
+                                    }
+
+                                    goto case "2";
+                                }
+                                else
+                                    break;
+                            }  
+                        }
+
+                        break;
+                    //multiplication of numbers
+                    case "3":
+                        {
+                            Console.ResetColor();
+                            Console.WriteLine("You CHOSE (3)");
+                            Console.WriteLine("multiplication of numbers");
+                            Console.WriteLine("Enter number A");
+                            decimal number1 = check();
+
+                            Console.WriteLine("Enter number B");
+                            decimal number2 = check();
+                            result[si] = number1 * number2;
+                            si = si + 1;
+                           
+                            if (si > 4)
+                            {
+                                si = 0;
+                            }
+
+                            Console.WriteLine($"result: {number1} * {number2} = {number1 * number2}");
+                            Console.WriteLine("Do you want to repeat enter? yes \nto exit press any button and press enter");
+                            String repeat = Console.ReadLine();
+
+                            while (true)
+                            {
+                                if (repeat == "yes")
+                                {
+
+                                    if (si > 4)
+                                    {
+                                        si = 0;
+                                    }
+
+                                    goto case "3";
+                                }
+                                else
+                                    break;
+                            }
+                        } 
+                        
+                        break;
+                    //quotient numbers
+                    case "4":
+                        {
+                            Console.ResetColor();
+                            Console.WriteLine("You CHOSE (4)");
+                            Console.WriteLine("quotient numbers");
+                            Console.WriteLine("Enter number A");
+                            decimal number1 = check();
+
+                            Console.WriteLine("Enter number B");
+                            decimal number2 = check();
+                            result[si] = number1 / number2;
+                            si = si + 1;
+
+                            if (si > 4)
+                            {
+                                si = 0;
+                            }
+
+                            Console.WriteLine($"result: {number1} / {number2} = {number1 / number2}");
+                            Console.WriteLine("Do you want to repeat enter? yes \nto exit press any button and press enter");
+                            String repeat = Console.ReadLine();
+
+                            while (true)
+                            {
+                                if (repeat == "yes")
+                                {
+
+                                    if (si > 4)
+                                    {
+                                        si = 0;
+                                    }
+
+                                    goto case "4";
+                                }
+                                else
+                                    break;
+                            }
+                        }
+
+                        break;
+                    //percentage of the number
+                    case "5":
+                        {
+                            Console.ResetColor();
+                            Console.WriteLine("You CHOSE (5)");
+                            Console.WriteLine("procent");
+                            Console.WriteLine("Enter number");
+                            decimal number1 = check();
+
+                            Console.WriteLine("enter any procent");
+                            decimal number2 = check();
+                            result[si] = (number1 / number2)/100;
+                            si = si + 1;
+                          
+                            if (si > 4)
+                            {
+                                si = 0;
+                            }
+
+                            Console.WriteLine($"result:  {(number1 * number2) / 100}");
+                            Console.WriteLine("Do you want to repeat enter? yes \nto exit press any button and press enter");
+                            String repeat = Console.ReadLine();
+
+                            while (true)
+                            {
+                                if (repeat == "yes")
+                                {
+
+                                    if (si > 4)
+                                    {
+                                        si = 0;
+                                    }
+
+                                    goto case "5";
+                                }
+                                else
+                                    break;
+                            }
+                        }
+
+                        break;
+                    //root of number
+                    case "6":
+                        {
+                            Console.ResetColor();
+                            Console.WriteLine("You CHOSE (5)");
+                            Console.WriteLine("√");
+                            Console.WriteLine("Enter number ");
+                            double number1;
+
+                            while (true)
+                            {
+                                if (double.TryParse(Console.ReadLine(), out number1))
+                                {
+                                    break;
+                                }
+                                else Console.WriteLine("Error    :enter number");
+                            }
+
+                            double number2 = Math.Sqrt(number1);
+                            Console.WriteLine($"Root from number = {number2}");
+                            decimal number3 = Convert.ToDecimal(number2);
+                            result[si] = number3;
+                            si = si + 1;
+
+                            if (si > 4)
+                            {
+                                si = 0;
+                            }
+
+                            Console.WriteLine("Do you want to repeat enter? yes \nto exit press any button and press enter");
+                            String repeat = Console.ReadLine();
+
+                            while (true)
+                            {
+
+                                if (repeat == "yes")
+                                {
+
+                                    if (si > 4)
+                                    {
+                                        si = 0;
+                                    }
+
+                                    goto case "6";
+                                }
+                                else
+                                    break;
+                            }
+                        }
+
+                        break;
+                    //last 5 operations on numbers
+                    case "7":
+                        {
+                            Console.WriteLine($"{result[0]}, {result[1]}, {result[2]}, {result[3]}, {result[4]}");
+                            Console.ReadKey();
+                        }
+
+                        break;
+                    //exit to programm
+                    case "8":
+                        {
+                            Console.WriteLine("Exit programm y -yes n-no?");
+                            String exit = Console.ReadLine();
+
+                            if (exit == "n")
+                            {
+                                break;
+                            }
+
+                            else if (exit == "y")
+                            {
+                                Environment.Exit(0);
+                            }
+
+                            else
+                            {
+                                Console.Clear();
+                                goto case "8";
+                            }
+                        }
+                        
+                        break;
+                    //if no menu button is selected
+                    default:
+                        {
+                            Console.WriteLine("Mistake. Make your choice \npress any button to continue");
+                            Console.ReadKey();
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
